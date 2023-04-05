02e834aa800b47f771753f725cfd7af78df46990
yjin84 <boev.evgenyj@gmail.com>
2023-04-05 21:55:04 +0700

    hw10 added homework (#10)
    * hw10 added homework
    
    * hw10 fix after review
    
    ---------
    
    Co-authored-by: yjin84 <yjin84@yandex.ru>

---
+++ hw10/src/main/java/ru/otus/crm/model/Address.java
@@ -0,0 +1,31 @@
+package ru.otus.crm.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "address")
+public class Address implements Cloneable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "street")
+    private String street;
+
+    @Override
+    public Address clone() {
+        return new Address(this.id, this.street);
+    }
+}
---
+++ hw10/src/main/java/ru/otus/crm/model/Client.java
@@ -0,0 +1,68 @@
+package ru.otus.crm.model;
+
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "client")
+public class Client implements Cloneable {
+
+    @Id
+    @SequenceGenerator(name = "client_gen", sequenceName = "client_seq",
+            initialValue = 1, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "client_gen")
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "client", orphanRemoval = true)
+    private List<Phone> phones;
+
+    public Client(String name) {
+        this.id = null;
+        this.name = name;
+    }
+
+    public Client(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Client(Long id, String name, Address address, List<Phone> phones) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.phones = phones;
+
+        for (Phone phone : this.phones) {
+            phone.setClient(this);
+        }
+    }
+
+    @Override
+    public Client clone() {
+        return new Client(this.id, this.name, this.address != null ? this.address.clone() : null, this.phones != null ? phones.stream().map(Phone::clone).toList() : null);
+    }
+}
