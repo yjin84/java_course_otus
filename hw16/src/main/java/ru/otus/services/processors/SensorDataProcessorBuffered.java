464054992944ee330b99805fe9eceb5c35f1a2d9
yjin84 <boev.evgenyj@gmail.com>
2023-05-27 21:25:31 +0700

    hw16 added homework (#16)
    * hw16 added homework
    
    * hw16 fix after review
    
    ---------
    
    Co-authored-by: yjin84 <yjin84@yandex.ru>

---
+++ hw16/src/main/java/ru/otus/services/processors/SensorDataProcessorBuffered.java
@@ -0,0 +1,63 @@
+package ru.otus.services.processors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.otus.api.SensorDataProcessor;
+import ru.otus.api.model.SensorData;
+import ru.otus.lib.SensorDataBufferedWriter;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+// Этот класс нужно реализовать
+public class SensorDataProcessorBuffered implements SensorDataProcessor {
+    private static final Logger log = LoggerFactory.getLogger(SensorDataProcessorBuffered.class);
+
+    private final int bufferSize;
+    private final SensorDataBufferedWriter writer;
+
+    private final BlockingQueue<SensorData> dataBuffer;
+
+    public SensorDataProcessorBuffered(int bufferSize, SensorDataBufferedWriter writer) {
+        this.bufferSize = bufferSize;
+        this.writer = writer;
+        this.dataBuffer = new PriorityBlockingQueue<>(
+                bufferSize,
+                Comparator.comparing(SensorData::getMeasurementTime)
+        );
+    }
+
+    @Override
+    public void process(SensorData data) {
+        dataBuffer.add(data);
+
+        if (dataBuffer.size() >= bufferSize) {
+            flush();
+        }
+    }
+
+    public void flush() {
+        try {
+            List<SensorData> bufferedData = new ArrayList<>(bufferSize);
+
+            dataBuffer.drainTo(bufferedData);
+
+            if (bufferedData.isEmpty()) {
+                return;
+            }
+
+            writer.writeBufferedData(bufferedData);
+        } catch (Exception e) {
+            log.error("Ошибка в процессе записи буфера", e);
+        }
+    }
+
+    @Override
+    public void onProcessingEnd() {
+        flush();
+    }
+}
